e4b0cdb87960df11fb23e9bc937d1084727d6ffb
anna-averianova <ann.averyanova@gmail.com>
2013-11-04 16:15:35 +0100

    Adds writing out the size function

--- Lab-2/BlackJack.hs
+++ Lab-2/BlackJack.hs
@@ -10,8 +10,10 @@ import Wrapper
 {-
    size hand2
      = size (Add(Card(Numeric 2) Hearts))
-                (Add (Card Jack Spades) Empty)
-     = ...
+                (Add (Card Jack Spades) Empty) =
+     = 1 + size (Add (Card Jack Spades) Empty) =
+     = 1 + 1 + size Empty =
+     = 1 + 1 + 0 =
      = 2
 -}
 

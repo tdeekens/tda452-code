fdb258ae31096c2dffaa87cf07e6b6862359fb68
Tobias Deekens <nerd@tdeekens.name>
2013-11-05 15:35:23 +0100

    Adds latest changes after Lab 2A's submission.

--- Lab-2/BlackJack.hs
+++ Lab-2/BlackJack.hs
@@ -61,3 +61,32 @@ winner :: Hand -> Hand -> Player
 winner guest bank | not (gameOver guest) && value guest > value bank = Guest
                   | not (gameOver guest) && gameOver bank            = Guest
                   | otherwise                                        = Bank
+
+-- Merges two hands by putting the first hand on top of the second one
+(<+) :: Hand -> Hand -> Hand
+Empty         <+ h2 = h2
+(Add card h1) <+ h2 = (Add card (h1 <+ h2))
+
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc p1 p2 p3 = p1 <+ (p2 <+ p3) == (p1 <+ p2) <+ p3
+
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf p1 p2 = size (p1 <+ p2) == size p1 + size p2
+
+-- Returns a full deck of cards listing 52 cards in total
+--fullDeck :: Hand
+-- foreach 2..10 (is a Card) fullSuits
+-- foreach Rank fullSuits
+
+-- Returns a partial deck containing all suits for a card
+--fullSuits :: Card
+-- foreach Suit-type pair with Card
+
+h1 = (Add (Card Ace Spades) (Add (Card(Numeric 5) Diamonds) (Add (Card Jack Diamonds) (Add (Card Ace Spades) Empty))))
+h2 = (Add (Card(Numeric 5) Diamonds) (Add (Card Jack Diamonds) (Add (Card Ace Spades) Empty)))
+h3 = (Add (Card(Numeric 10) Diamonds) (Add (Card Jack Diamonds) (Add (Card Jack Spades) Empty)))
+h4 = (Add (Card Ace Spades) (Add (Card Jack Diamonds) (Add (Card Jack Spades) Empty)))
+
+h5 = (Add (Card(Numeric 5) Diamonds) Empty)
+h6 = (Add (Card Jack Diamonds) (Add (Card Ace Spades) Empty))
+h7 = (Add (Card(Numeric 7) Spades) Empty)

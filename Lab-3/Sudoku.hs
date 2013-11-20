cd5959f540f2cba918c84cc7800f5917c08668e7
Tobias Deekens <nerd@tdeekens.name>
2013-11-20 16:08:57 +0100

    Adds changes due to hlint and documentation.

--- Lab-3/Sudoku.hs
+++ Lab-3/Sudoku.hs
@@ -5,17 +5,21 @@ import Data.Maybe
 import Data.Char
 import Data.List
 
+{-
+   Lab Assignment 3
+   Anna Averianova & Tobias Deekens, 2013
+-}
+
 -------------------------------------------------------------------------
 
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
  deriving ( Show, Eq )
 
--- allBlankSudoku is a sudoku with just blanks
+-- creates a sudoku with only blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (replicate 9 (replicate 9 Nothing))
 
--- isSudoku sud checks if sud is really a valid representation of a sudoku
--- puzzle
+-- checks if sud is really a valid representation of a sudoku puzzle
 isSudoku :: Sudoku -> Bool
 isSudoku s = rowCount == 9 && all isValidRow rows'
   where
@@ -26,45 +30,53 @@ isSudoku s = rowCount == 9 && all isValidRow rows'
       = length (filter
           (\ x -> isNothing x || fromJust x >= 1 && fromJust x <= 9) r) == 9
 
--- isSolved sud checks if sud is already solved, i.e. there are no blanks
+-- checks if a sud has no blanks
 isSolved :: Sudoku -> Bool
 isSolved s = isSudoku s && all isRowSolved rows'
   where
     rows' = rows s
-    isRowSolved = all isNothing -- as field boundaries are checked by isSudoku
+    isRowSolved = all $ not . isNothing
 
 -------------------------------------------------------------------------
 
--- printSudoku sud prints a representation of the sudoku sud on the screen
+-- prints a representation of a sud on the screen
 printSudoku :: Sudoku -> IO ()
 printSudoku s = putStr (unlines (map printRow rows'))
   where
     rows' = rows s
-    printRow :: [Maybe Int] -> String
-    printRow []     = ""
-    printRow (x:xs) = printCell x ++ printRow xs
-    printCell :: Maybe Int -> String
-    printCell Nothing = "."
-    printCell cell    = show $ fromJust cell
 
--- readSudoku file reads from the file, and either delivers it, or stops
+-- helper printing a list of sud cells on the screen
+printRow :: [Maybe Int] -> String
+printRow = foldr ((++) . printCell) ""
+
+-- helper printing a sud cell on the screen
+printCell :: Maybe Int -> String
+printCell Nothing = "."
+printCell cell    = show $ fromJust cell
+
+-- reads from the file, and either delivers it, or stops
 -- if the file did not contain a sudoku
 readSudoku :: FilePath -> IO Sudoku
 readSudoku fp = do
                   contents <- readFile fp
                   return ( parseSudoku contents )
-  where
-    parseSudoku :: String -> Sudoku
-    parseSudoku s = Sudoku (map parseRow (lines s))
-    parseRow :: String -> [Maybe Int]
-    parseRow = map parseCell
-    parseCell :: Char -> Maybe Int
-    parseCell c | c == '.'  = Nothing
-                | otherwise = Just (ord c - ord '0')
+
+-- parses a sud from a string
+parseSudoku :: String -> Sudoku
+parseSudoku s = Sudoku (map parseRow (lines s))
+
+-- parses a sud row from a string
+parseRow :: String -> [Maybe Int]
+parseRow = map parseCell
+
+-- parses a sud cell from a char
+parseCell :: Char -> Maybe Int
+parseCell c | c == '.'  = Nothing
+            | otherwise = Just (ord c - ord '0')
 
 -------------------------------------------------------------------------
 
--- cell generates an arbitrary cell in a Sudoku
+-- generates an arbitrary sud cell
 cell :: Gen (Maybe Int)
 cell = frequency
          [(5, return Nothing),
@@ -76,7 +88,7 @@ instance Arbitrary Sudoku where
     do rows <- sequence [ sequence [ cell | j <- [1..9] ] | i <- [1..9] ]
        return (Sudoku rows)
 
--- property indicating if a Sudoku is valid
+-- property indicating if a sud is valid
 prop_Sudoku :: Sudoku -> Bool
 prop_Sudoku = isSudoku
 
@@ -84,42 +96,41 @@ prop_Sudoku = isSudoku
 
 type Block = [Maybe Int]
 
--- checks if a block is valid in following sudoku rules
+-- checks if a block is valid following the sud rules
 isOkayBlock :: Block -> Bool
 isOkayBlock b = length withoutNothing == length (nub withoutNothing)
   where
     withoutNothing = filter isJust b
 
--- generates blocks from a sudoku containing rows, columns and block lists
+-- returns rows, columns and blocks of a sud
 blocks :: Sudoku -> [Block]
 blocks s = rows' ++ columns' ++ blocks'
   where
     rows'    = rows s
     columns' = transpose rows'
-    blocks'  = filter(\x -> x/=[]) (rowWalker rows')
+    blocks'  = filter(not . null) (rowWalker rows')
 
--- helper function walking the sudoku's rows in steps of three
+-- helper function walking the sud's rows three at a time
 rowWalker :: [[Maybe Int]] -> [[Maybe Int]]
-rowWalker r | length r == 3 = columnWalker $ take 3 r
-            | otherwise     = columnWalker (take 3 r) ++ rowWalker (drop 3 r)
+rowWalker []  = [[]]
+rowWalker r   = columnWalker (take 3 r) ++ rowWalker (drop 3 r)
 
--- helper function walking the sudoku's columns in steps of three creating list of blocks
+-- helper function returning a list of blocks given three rows
 columnWalker :: [[Maybe Int]] -> [[Maybe Int]]
-columnWalker ([]:[]:[]:[]) = [[]]
-columnWalker (x:y:z:ws)    = ((take 3 x) ++ (take 3 y) ++ (take 3 z)) :
-                                columnWalker ((drop 3 x) : (drop 3 y) : (drop 3 z) : ws)
+columnWalker (x:y:z:ws)
+  | null x = [[]]
+  | otherwise = ((take 3 x) ++ (take 3 y) ++ (take 3 z)) :
+                  columnWalker ((drop 3 x) : (drop 3 y) : (drop 3 z) : ws)
 
 -- validates if a sudoku is valid (not solved)
 isOkay :: Sudoku -> Bool
 isOkay s = all isOkayBlock (blocks s)
 
--- property validating sizes of blocks and their cells' length
+-- property validating the amount of blocks and their sizes
 prop_blocks :: Sudoku -> Bool
 prop_blocks s = length blocks' == 27 && all (\b -> length b == 9) blocks'
   where
     blocks' = blocks s
-
--------------------------------------------------------------------------
 
 -- Example Sudoku
 example :: Sudoku

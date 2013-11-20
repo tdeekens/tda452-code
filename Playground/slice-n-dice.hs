38f41016509ccd5f1e37fd4e64ea5ccc7f8247df
Tobias Deekens <nerd@tdeekens.name>
2013-11-20 13:08:28 +0100

    Updates playground adding chunks''.

--- Playground/slice-n-dice.hs
+++ Playground/slice-n-dice.hs
@@ -1,8 +1,9 @@
 module SliceNDice where
 
 -- sample data
-nested = [ [1..6], [7..12], [13..18] ]
-list   = [1..10]
+nested  = [ [1..6], [7..12], [13..18] ]
+nested2 = [ [1..6], [7..12], [13..18], [19..24] ]
+list    = [1..10]
 
 -- slices a part of a list starting at `from` with `size`
 slice' :: Int -> Int -> [a] -> [a]
@@ -22,6 +23,15 @@ slice'' from size l = (head', tail')
 chunks' :: [[a]] -> Int -> [[a]]
 chunks' [[],[],[]] _ = []
 chunks' ls         s = [heads'] ++ (chunks' tails' s)
+   where
+      -- list of tuples in [(head, tail)...]
+      chopinception = map (slice'' 0 s) ls
+      heads'        = concat $ map fst chopinception
+      tails'        = map snd chopinception
+
+chunks'' :: [[a]] -> Int -> [[a]]
+chunks'' ls _ | null $ filter (not . null) ls = []
+chunks'' ls s | otherwise                     = [heads'] ++ (chunks'' tails' s)
    where
       -- list of tuples in [(head, tail)...]
       chopinception = map (slice'' 0 s) ls

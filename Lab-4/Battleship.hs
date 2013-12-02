b34861dda8e5169d5e1062e26db9abfe570b3e09
Tobias Deekens <nerd@tdeekens.name>
2013-12-02 15:34:08 +0100

    Adds Lab-4 and Battleship.hs.

---
+++ Lab-4/Battleship.hs
@@ -0,0 +1,12 @@
+module Battleship where
+
+import Test.QuickCheck
+import Data.Maybe
+import Data.List
+
+{-
+   Lab Assignment 3
+   Anna Averianova & Tobias Deekens, 2013
+-}
+
+-------------------------------------------------------------------------

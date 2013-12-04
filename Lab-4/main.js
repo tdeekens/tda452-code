f66075c4fbf8127b5cdc69353cbd6e5022dfae6f
Tobias Deekens <nerd@tdeekens.name>
2013-12-04 20:22:09 +0100

    Adds support for placing ships and flipping them.

---
+++ Lab-4/main.js
@@ -0,0 +1,134 @@
+window.battleship = {};
+
+battleship.config = {
+   log: true,
+   delimiter: "-",
+   start: [0, 0],
+   alignment: 'horizontal',
+   sizes: {
+      'aircraftcarrier': 5,
+      'battleship': 4,
+      'submarine': 3,
+      'destroyer': 3,
+      'patrolboat': 2
+   }
+}
+
+battleship.state = {
+   booked: [],
+   boatmodel: undefined,
+   start: undefined,
+   alignment: undefined
+};
+
+battleship.selectBoat = function() {
+   if (!true) { //tobe validation in haskell
+      return false;
+   }
+
+   var $this     = $(this);
+   var boatmodel = $this.data('model');
+
+   battleship.state.boatmodel = boatmodel;
+   battleship.state.start     = battleship.config.start;
+   battleship.state.alignment = battleship.config.alignment;
+
+   console.log('State changed:', battleship.state);
+};
+
+battleship.addBoat = function() {
+   if (battleship.state.boatmodel === undefined) {
+      alert("Please select a boat to position!");
+      return false;
+   }
+
+   battleship.markHorizontal(false);
+
+   var $this    = $(this)
+       , id       = $this.attr('id')
+       , position = id.split(battleship.config.delimiter);
+
+   battleship.state.start = [position[0]--, position[1]--];
+
+   battleship.markHorizontal(true);
+
+   console.log('State changed:', battleship.state);
+}
+
+battleship.markHorizontal = function(book) {
+   var boatmodel = battleship.state.boatmodel;
+
+   var row  = battleship.state.start[0] + 1
+       , cell = battleship.state.start[1] + 1;
+
+   var length = cell + battleship.config.sizes[boatmodel];
+   var $cells = $( $('tr').get( row ) ).find('td');
+
+   $cells = $cells.slice(cell, length);
+
+   if (book === true) {
+      $cells.addClass('boat');
+      $cells.data('model', boatmodel);
+   } else {
+      $cells.removeClass('boat')
+      $cells.data('model', null);
+   }
+}
+
+battleship.markVertically = function(book) {
+   var boatmodel = battleship.state.boatmodel;
+
+   var $rows     = $('tr')
+       , column = battleship.state.start[0] + 1;
+
+   var length = column + battleship.config.sizes[boatmodel];
+
+   $rows = $rows.slice(column, length);
+   if (book === true) {
+      $.each($rows, function(i, row) {
+         var $row = $(row);
+         var $cell = $( $row.find('td').get(column) );
+
+         $cell.addClass('boat').data('model', boatmodel);
+      });
+   } else {
+      $.each($rows, function(i, row) {
+         var $row = $(row);
+         var $cell = $( $row.find('td').get(column) );
+
+         $cell.removeClass('boat').data('model', null);
+      });
+   }
+}
+
+battleship.flipBoat = function() {
+   if(battleship.state.alignment === 'horizontal') {
+      battleship.state.alignment = 'vertical';
+      battleship.markHorizontal(false);
+      battleship.markVertically(true);
+   } else {
+      battleship.state.alignment = 'horizontal';
+      battleship.markVertically(false);
+      battleship.markHorizontal(true);
+   }
+
+   console.log('State changed:', battleship.state);
+};
+
+battleship.lockBoat = function() {
+   if (!true) { // ask haskell here
+      alert("Duplicate boat or invalid position!");
+      return false;
+   } else {
+      battleship.state.boatmodel = undefined;
+      battleship.state.start     = undefined;
+      battleship.state.alignment = undefined;
+   }
+};
+
+$(function() {
+   $('.boat').on('click', battleship.selectBoat);
+   $('td').on('click', battleship.addBoat);
+   $('button#flip').on('click', battleship.flipBoat);
+   $('button#lock').on('click', battleship.lockBoat);
+});

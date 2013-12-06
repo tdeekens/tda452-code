3516e795c6849d18887c3943b9d178c2a777ff75
Tobias Deekens <nerd@tdeekens.name>
2013-12-06 13:17:38 +0100

    Renames main.js to battleship.js.

--- Lab-4/ui.hs
+++ Lab-4/ui.hs
@@ -1,12 +1,2 @@
 import Haste
 import Battleship
-
-main :: IO ()
-main = undefined
-
-getShipModel :: String -> Model
-getShipModel "aircraftcarrier" = AircraftCarrier
-getShipModel "battleship"      = Battleship
-getShipModel "submarine"       = Submarine
-getShipModel "destroyer"       = Submarine
-getShipModel "patrolboat"      = PatrolBoat
